a303fe94923d9b79389c83d4b2e14e1086cd0048
aruninfrrd <arun@infrrd.ai>
2017-12-08 23:47:52 +0530

    initial commit

---
+++ src/main/java/com/infrrd/myspider/webspider/WebspiderApplication.java
@@ -0,0 +1,12 @@
+package com.infrrd.myspider.webspider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebspiderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebspiderApplication.class, args);
+	}
+}
